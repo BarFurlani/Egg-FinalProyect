54fdd6f667be39e8f2da5885fe6d0ca28fe8c3f2
Barby <117693057+BarFurlani@users.noreply.github.com>
2023-09-08 15:29:02 -0300

    Add files via upload

---
+++ src/main/java/com/quinchos/QuinchosApplication.java
@@ -0,0 +1,13 @@
+package com.quinchos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinchosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuinchosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quinchos/QuinchosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinchos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinchosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
