2eb1f7ede131a0cc5bf80ca98cd51aa4faab18f4
Rafael Quinones <rafaelquipar@gmail.com>
2023-09-17 19:33:24 -0500

    Clase Propiedad + CRUD

---
+++ src/main/java/com/EquipoB/AlquilerQuinchos/Repositorios/RepositorioPropiedad.java
@@ -0,0 +1,25 @@
+package com.EquipoB.AlquilerQuinchos.Repositorios;
+
+import com.EquipoB.AlquilerQuinchos.Entitades.Propiedad;
+import com.EquipoB.AlquilerQuinchos.Entitades.Usuario;
+import com.EquipoB.AlquilerQuinchos.Enumeraciones.TipoDePropiedad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RepositorioPropiedad extends JpaRepository<Propiedad, Long> {
+
+    Optional<Propiedad> findByNombre(String nombre);
+
+    List<Propiedad> findByUbicacion(String ubicacion);
+
+    List<Propiedad> findByTipoDePropiedad(TipoDePropiedad tipoDePropiedad);
+
+    List<Propiedad> findByPropietario(Usuario propietario);
+
+    Optional<Propiedad> findByInquilino(Usuario inquilino);
+
+}
